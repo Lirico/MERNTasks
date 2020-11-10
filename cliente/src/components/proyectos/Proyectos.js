3a0228865106e6a0ab97d2200edaa25401dc8974
Lirico <dominguezmatiasadrian@gmail.com>
2020-11-10 18:45:35 -0300

    version 4

--- cliente/src/components/proyectos/Proyectos.js
+++ cliente/src/components/proyectos/Proyectos.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Sidebar from '../layout/Sidebar';
+import Barra from '../layout/Barra';
+import FromTarea from '../tareas/FormTarea';
+import ListadoTareas from '../tareas/ListadoTareas';
 
 const Proyectos = () => {
     return ( 
-        <h1>Desde Proyectos</h1>
+        <div className="contenedor-app">
+            <Sidebar />
+            <div className="seccion-principal">
+                <Barra />
+                
+                <main>
+                    <FromTarea />
+                    <div className="contenedor-tareas"> 
+                        <ListadoTareas />
+                    </div>
+                </main>
+            </div>
+        </div>
      );
 }
  
