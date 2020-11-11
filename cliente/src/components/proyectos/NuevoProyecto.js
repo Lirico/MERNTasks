2608f34f21d394c3c4e9373f7aed418d54899ebb
Lirico <dominguezmatiasadrian@gmail.com>
2020-11-11 18:41:45 -0300

    version 5

--- cliente/src/components/proyectos/NuevoProyecto.js
+++ cliente/src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,11 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const NuevoProyecto = () => {
+
+    // Obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const { formulario, errorformulario, mostrarFormulario, agregarProyecto, mostrarError } = proyectosContext;
 
     // State para Proyecto
     const [ proyecto, guardarProyecto] = useState({
@@ -22,12 +27,24 @@ const NuevoProyecto = () => {
         e.preventDefault();
 
         // Validar el proyecto
+        if (nombre === '') {
+            mostrarError();
+            return;
+        }
 
         // agregar al state
+        agregarProyecto(proyecto)
 
         // Reiniciar el form
+        guardarProyecto({
+            nombre: ''
+        })
     }
-     
+    
+    // Mostrar el formulario
+    const onClickFormulario = () => {
+        mostrarFormulario();
+    }
     
 
     return ( 
@@ -35,27 +52,32 @@ const NuevoProyecto = () => {
             <button
                 type="button"
                 className="btn btn-block btn-primario"
+                onClick={ onClickFormulario }
             >Nuevo Proyecto</button>
 
-            <form
-                className="formulario-nuevo-proyecto"
-                onSubmit={onSubmitProyecto}
-            >
-                <input 
-                    type="text"
-                    className="input-text"
-                    placeholder="Nombre Proyecto"
-                    name="nombre"
-                    value={nombre}
-                    onChange={onChangeProyecto}
-                />
+            { formulario ? 
+                    (
+                        <form
+                            className="formulario-nuevo-proyecto"
+                            onSubmit={onSubmitProyecto}
+                        >
+                            <input 
+                                type="text"
+                                className="input-text"
+                                placeholder="Nombre Proyecto"
+                                name="nombre"
+                                value={nombre}
+                                onChange={onChangeProyecto}
+                            />
 
-                <input
-                    type="submit"
-                    className="btn btn-primario btn-block"
-                    value="Agregar Proyecto" 
-                />
-            </form>
+                            <input
+                                type="submit"
+                                className="btn btn-primario btn-block"
+                                value="Agregar Proyecto" 
+                            />
+                        </form>
+                    ) : null }
+                { errorformulario ? <p className="mensaje error">El nombre del Proyecto es obligatorio</p> : null }
         </Fragment>
      );
 }
--- cliente/src/components/tareas/ListadoTareas.js
+++ cliente/src/components/tareas/ListadoTareas.js
@@ -1,7 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoTareas = () => {
+
+    // Extraer proyecto de state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto, eliminarProyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>
+
+    // Array destructuring para extraer el proyecto actual
+    const [ proyectoActual ] = proyecto;
 
     const tareasProyecto = [
         { nombre: 'Elegir Plataforma', estado: true},
@@ -10,10 +21,15 @@ const ListadoTareas = () => {
         { nombre: 'Elegir Hosting', estado: true}
     ]
 
+    // Eliminar un proyecto
+    const onClickEliminar = () => {
+        eliminarProyecto(proyectoActual.id)
+    }
+
     return ( 
 
         <Fragment>
-            <h2>Proyecto: Tienda Virtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre}</h2>
 
             <ul className="listado-tareas">
                 {tareasProyecto.length === 0 
@@ -29,6 +45,7 @@ const ListadoTareas = () => {
             <button
                 type="button"
                 className="btn btn-eliminar"
+                onClick={onClickEliminar}
             >Eliminar Proyecto &times;</button>
         </Fragment>
      );
